0cb89ba005f7b42c9b07ce76382e401fd70d7301
Kenneth Yang <kjy@is88.com>
2026-06-21 15:26:10 -0700

    Draft shader, but it crashes after some time

--- src/scenes/MyFirstScene.ts
+++ src/scenes/MyFirstScene.ts
@@ -9,6 +9,7 @@ import {
   RawTexture,
   RawTexture3D,
   Scene,
+  ShaderStore,
   StorageBuffer,
   Texture,
   TransformNode,
@@ -75,13 +76,16 @@ const createScene = async (
 
   // Build LUT texture.
   const lutData = new Uint8Array([
-    255, 0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255, 255, 255, 255, 255,
+    255, 0, 0, 255,
+    0, 255, 0, 255,
+    0, 0, 255, 255,
+    0, 0, 0, 255,
   ])
   const lutTexture = new RawTexture(
     lutData,
-    3,
+    4,
     1,
-    Engine.TEXTUREFORMAT_RGBA,
+    Engine.TEXTUREFORMAT_RGBA_INTEGER,
     scene,
     false,
     false,
@@ -91,31 +95,110 @@ const createScene = async (
 
   // Build input parameter buffer.
   const sliceParameterBuffer = new UniformBuffer(engine, undefined, true, 'parameters')
-  sliceParameterBuffer.addUniform('center', 3)
-  sliceParameterBuffer.addUniform('size', 1)
-  sliceParameterBuffer.addUniform('up', 3)
-  sliceParameterBuffer.addUniform('resolution', 1)
-  sliceParameterBuffer.addUniform('right', 3)
-  sliceParameterBuffer.addUniform('startCoordinate', 3)
-  sliceParameterBuffer.addUniform('endCoordinate', 3)
+  sliceParameterBuffer.addUniform('centerAndHalfSize', 4)
+  sliceParameterBuffer.addUniform('rightAndChunkResolution', 4)
+  sliceParameterBuffer.addUniform('up', 4)
+  sliceParameterBuffer.addUniform('chunkStartCoordinate', 4)
 
-  sliceParameterBuffer.updateFloat3('center', 1.5, 1.5, 1.5)
-  sliceParameterBuffer.updateUInt('size', 2)
-  sliceParameterBuffer.updateFloat3('up', 0, 0, 1)
-  sliceParameterBuffer.updateUInt('resolution', 100)
-  sliceParameterBuffer.updateFloat3('right', 1, 0, 0)
-  sliceParameterBuffer.updateFloat3('startCoordinate', 0, 0, 0)
-  sliceParameterBuffer.updateFloat3('endCoordinate', 400, 400, 400)
+  sliceParameterBuffer.updateFloat4('centerAndHalfSize', 0.15, 0.15, 0.15, 0.2)
+  sliceParameterBuffer.updateFloat4('rightAndChunkResolution', 1, 0, 0, 0.1)
+  sliceParameterBuffer.updateFloat4('up', 0, 0, 1, 0)
+  sliceParameterBuffer.updateFloat4('chunkStartCoordinate', 0, 0, 0, 0)
 
   sliceParameterBuffer.update()
 
   // Declare output buffers.
-  const sliceColorBuffer = new StorageBuffer(
+  const OUTPUT_SIZE = 500
+  const sliceIdBuffer = new StorageBuffer(
     engine,
-    64 * 4,
+    OUTPUT_SIZE * OUTPUT_SIZE * 4,
     Constants.BUFFER_CREATIONFLAG_READWRITE,
   )
-  const sliceIdBuffer = new StorageBuffer(engine, 64 * 4, Constants.BUFFER_CREATIONFLAG_READWRITE)
+  const sliceColorBuffer = new StorageBuffer(
+    engine,
+    OUTPUT_SIZE * OUTPUT_SIZE * 4,
+    Constants.BUFFER_CREATIONFLAG_READWRITE,
+  )
+
+  // Store shader.
+  ShaderStore.ShadersStoreWGSL['sliceComputeShader'] = `
+  // Input paramters. In world space mm.
+  struct Parameters {
+      centerAndHalfSize: vec4f,
+      rightAndChunkResolution: vec4f,
+      up: vec4f,
+      chunkStartCoordinate: vec4f,
+  }
+
+    // Input paramters.
+    @group(0) @binding(0) var<uniform> parameters: Parameters;
+
+    // Atlas annotation volume chunk.
+    @group(0) @binding(1) var annotationChunk: texture_3d<u32>;
+
+    // Annotation IDs to color LUT.
+    @group(0) @binding(2) var lut: texture_2d<u32>;
+
+    // Slice IDs.
+    @group(0) @binding(3) var<storage, read_write> idOut: array<u32>;
+
+    // Slice colors (8-bit RGBA).
+    @group(0) @binding(4) var<storage, read_write> colorOut: array<u32>;
+
+    @compute @workgroup_size(8, 8, 1)
+  fn main(@builtin(global_invocation_id) globalThreadId: vec3u) {
+      // Extract output pixel coordinate.
+      let outputSideLength = u32(sqrt(f32(arrayLength(&colorOut))));
+      let pixelX = globalThreadId.x;
+      let pixelY = globalThreadId.y;
+      let pixelIndex = pixelY * outputSideLength + pixelX;
+      if pixelX >= outputSideLength || pixelY >= outputSideLength { return; }
+
+      // Unpack parameters.
+      let center = parameters.centerAndHalfSize.xyz;
+      let halfSize = parameters.centerAndHalfSize.w;
+      let right = parameters.rightAndChunkResolution.xyz;
+      let chunkResolution = parameters.rightAndChunkResolution.w;
+      let up = parameters.up.xyz;
+      let chunkStartCoordinate = parameters.chunkStartCoordinate.xyz;
+
+      // Compute world space coordiante of pixel.
+      let normalizedS = (f32(pixelX) + 0.5) / f32(outputSideLength) * 2.0 - 1.0;
+      let normalizedT = (f32(pixelY) + 0.5) / f32(outputSideLength) * 2.0 - 1.0;
+      let worldPosition = center + normalizedS * right * halfSize + normalizedT * up * halfSize;
+
+      // Extract chunk dimensions (used later).
+      let chunkDimensions = textureDimensions(annotationChunk);
+      let chunkEndCoordinate = chunkStartCoordinate + vec3f(chunkDimensions) * chunkResolution;
+
+      // Exit if position out of chunk bounds.
+      if any(worldPosition < chunkStartCoordinate) || any(worldPosition >= chunkEndCoordinate) {
+          idOut[pixelIndex] = 0u;
+          colorOut[pixelIndex] = 0u;
+          return;
+      }
+
+      // Compute annotation index in chunk.
+      let chunkLocalPosition = worldPosition - chunkStartCoordinate;
+      let annotationChunkIndex = vec3i(floor(chunkLocalPosition / chunkResolution));
+
+      // Verify bounds and load ID (default to empty).
+      var structureId: u32 = 0u;
+      if all(annotationChunkIndex >= vec3i(0)) && all(annotationChunkIndex < vec3i(chunkDimensions)) {
+          structureId = textureLoad(annotationChunk, annotationChunkIndex, 0).r;
+      }
+
+      // Write ID to ID output.
+      idOut[pixelIndex] = structureId;
+
+      // Look up color in LUT.
+      let lutIndex = select(0u, structureId, structureId < textureDimensions(lut).x);
+      let lutColor = textureLoad(lut, vec2i(i32(lutIndex), 0), 0);
+
+      // Write to color output (repack expanded load).
+      colorOut[pixelIndex] = lutColor.r | (lutColor.g << 8u) | (lutColor.b << 16u) | (lutColor.a << 24u);
+  }
+  `
 
   // Declare compute shader and bind data.
   const sliceComputeShader = new ComputeShader('sliceComputeShader', engine, 'slice', {
@@ -123,19 +206,26 @@ const createScene = async (
       parameters: { group: 0, binding: 0 },
       annotationChunk: { group: 0, binding: 1 },
       lut: { group: 0, binding: 2 },
-      colorOut: { group: 0, binding: 3 },
-      idOut: { group: 0, binding: 4 },
+      idOut: { group: 0, binding: 3 },
+      colorOut: { group: 0, binding: 4 },
     },
   })
   sliceComputeShader.setUniformBuffer('parameters', sliceParameterBuffer)
   sliceComputeShader.setTexture('annotationChunk', annotationChunkTexture, false)
   sliceComputeShader.setTexture('lut', lutTexture, false)
-  sliceComputeShader.setStorageBuffer('colorOut', sliceColorBuffer)
   sliceComputeShader.setStorageBuffer('idOut', sliceIdBuffer)
+  sliceComputeShader.setStorageBuffer('colorOut', sliceColorBuffer)
 
-  // Configure render canvas.
-  const sliceCanvasOffscreen = sliceCanvas.transferControlToOffscreen()
-  // TODO: Spawn render worker.
+  // Dispatch with workgroups.
+  const WORKGROUPS = Math.ceil(OUTPUT_SIZE / 8)
+  sliceComputeShader.dispatchWhenReady(WORKGROUPS, WORKGROUPS, 1).then(() => {
+    sliceColorBuffer.read().then((colorData) => {
+      const pixels = new Uint8ClampedArray(colorData.buffer as ArrayBuffer)
+      sliceCanvas
+        .getContext('2d')
+        ?.putImageData(new ImageData(pixels, OUTPUT_SIZE, OUTPUT_SIZE), 0, 0)
+    })
+  })
 
   // Engine render loop.
   engine.runRenderLoop(() => {
