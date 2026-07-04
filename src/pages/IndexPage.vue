32c41e5570d06f0fc0ce812782ea31f801c24d95
Kenneth Yang <kjy@is88.com>
2026-07-03 20:02:12 -0700

    Left drawer tabs

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,39 +1,49 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const leftDrawerOpen = ref(false);
+const rightDrawerOpen = ref(false);
+const tab = ref("scene");
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+function toggleRightDrawer() {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+}
+</script>
+
 <template>
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Pinpoint V </q-toolbar-title>
+        <q-toolbar-title> Pinpoint V</q-toolbar-title>
 
         <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+      <q-tabs v-model="tab">
+        <q-tab name="scene" label="Scene" />
+        <q-tab name="channel-maps" label="Channel Maps" />
+        <q-tab name="atlas" label="Atlas" />
+      </q-tabs>
+      <q-separator />
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="scene"> Scene </q-tab-panel>
+        <q-tab-panel name="channel-maps"> Channel maps </q-tab-panel>
+        <q-tab-panel name="atlas"> Atlas </q-tab-panel>
+      </q-tab-panels>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
       <!-- drawer content -->
     </q-drawer>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+    <q-page-container> Hello World </q-page-container>
   </q-layout>
 </template>
-
-<script setup>
-import { ref } from "vue";
-
-const leftDrawerOpen = ref(false);
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
-
-const rightDrawerOpen = ref(false);
-function toggleRightDrawer() {
-  rightDrawerOpen.value = !rightDrawerOpen.value;
-}
-</script>
