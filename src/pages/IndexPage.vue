249b1be044e02a25f2f20fb1a9350139c4bc5ba2
Kenneth Yang <kjy@is88.com>
2026-07-04 17:10:40 -0700

    Resizable drawers

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,19 +7,45 @@
 
 import { ref } from "vue";
 import { SceneCanvas } from "@/features/scene";
+import { TouchPanValue } from "quasar";
 
+// Layout state.
 const leftDrawerOpen = ref(false);
 const rightDrawerOpen = ref(false);
+const leftDrawerWidth = ref(350);
+const rightDrawerWidth = ref(350);
 const tab = ref("scene");
 const appVersion = import.meta.env.APP_VERSION;
 
+/**
+ * Toggle left drawer open state.
+ */
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 
+/**
+ * Toggle right drawer open state.
+ */
 function toggleRightDrawer() {
   rightDrawerOpen.value = !rightDrawerOpen.value;
 }
+
+/**
+ * Change the size of the left drawer based on the touch pan directive.
+ * @param details Touch pan directive values.
+ */
+const resizeLeftDrawer: TouchPanValue = function (details) {
+  leftDrawerWidth.value += details.delta?.x ?? 0;
+};
+
+/**
+ * Change the size of the right drawer based on the touch pan directive.
+ * @param details Touch pan directive values.
+ */
+const resizeRightDrawer: TouchPanValue = function (details) {
+  rightDrawerWidth.value -= details.delta?.x ?? 0;
+};
 
 /**
  * Force minHeight and height of QPage to be the same.
@@ -71,7 +97,13 @@ function fixedQPageHeight(offset: number) {
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      :width="leftDrawerWidth"
+      bordered
+      show-if-above
+      side="left"
+    >
       <q-tabs v-model="tab">
         <q-tab name="scene" label="Scene" />
         <q-tab name="channel-maps" label="Channel Maps" />
@@ -83,10 +115,24 @@ function fixedQPageHeight(offset: number) {
         <q-tab-panel name="channel-maps"> Channel maps </q-tab-panel>
         <q-tab-panel name="atlas"> Atlas </q-tab-panel>
       </q-tab-panels>
+      <div
+        v-touch-pan.horizontal.prevent.mouse="resizeLeftDrawer"
+        class="q-drawer__resizer q-drawer__resizer--left"
+      ></div>
     </q-drawer>
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+    <q-drawer
+      v-model="rightDrawerOpen"
+      :width="rightDrawerWidth"
+      bordered
+      show-if-above
+      side="right"
+    >
       <!-- drawer content -->
+      <div
+        v-touch-pan.horizontal.prevent.mouse="resizeRightDrawer"
+        class="q-drawer__resizer q-drawer__resizer--right"
+      ></div>
     </q-drawer>
 
     <q-page-container>
@@ -96,3 +142,30 @@ function fixedQPageHeight(offset: number) {
     </q-page-container>
   </q-layout>
 </template>
+
+<style lang="sass" scoped>
+.q-drawer__resizer
+  position: absolute
+  top: 0
+  bottom: 0
+  width: 3px
+  background-color: lightgray
+  cursor: ew-resize
+
+  &:after
+    content: ''
+    position: absolute
+    top: 50%
+    height: 10%
+    left: -2px
+    right: -2px
+    transform: translateY(-50%)
+    background-color: inherit
+    border-radius: 4px
+
+.q-drawer__resizer--left
+  right: -1px
+
+.q-drawer__resizer--right
+  left: -1px
+</style>
