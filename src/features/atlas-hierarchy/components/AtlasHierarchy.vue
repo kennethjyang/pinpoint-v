21d3c1cd5f44b43b599eefd6eedb3fd810b637c6
Kenneth Yang <kjy@is88.com>
2026-07-12 10:10:25 -0700

    Build hierarchy

--- src/features/atlas-hierarchy/components/AtlasHierarchy.vue
+++ src/features/atlas-hierarchy/components/AtlasHierarchy.vue
@@ -1,8 +1,46 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useCurrentAtlas } from "@/composable/useCurrentAtlas";
+import { AtlasStructure } from "@/models/atlas-metadata.model";
+
+interface TreeModel {
+  label: string;
+  children: TreeModel[];
+}
+
+const currentAtlas = useCurrentAtlas();
 
 const filter = ref<string | null>(null);
-const hierarchy = ref<[]>([]);
+const hierarchy = ref<TreeModel[]>([]);
+
+watch(
+  currentAtlas.metadata,
+  metadata => {
+    const { rootId, structures } = metadata ?? {};
+    if (!rootId || !structures || !structures[rootId]) return;
+
+    hierarchy.value = [buildHierarchyEntry(structures[rootId], structures)];
+  },
+  { immediate: true }
+);
+
+function buildHierarchyEntry(
+  structure: AtlasStructure,
+  structures: AtlasStructure[]
+): TreeModel {
+  // Only include acronym if it's different.
+  const label =
+    structure.name === structure.acronym
+      ? structure.name
+      : `${structure.name} (${structure.acronym.toUpperCase()})`;
+
+  return {
+    label,
+    children: structure.childrenIds.flatMap(id =>
+      structures[id] ? [buildHierarchyEntry(structures[id], structures)] : []
+    )
+  };
+}
 </script>
 
 <template>
