bef0179b6b2ed9ad823160041d125f8712f99052
Kenneth Yang <kjy@is88.com>
2026-07-11 22:44:01 -0700

    Build a list of paths to the default structures

--- src/services/atlas.service.ts
+++ src/services/atlas.service.ts
@@ -7,26 +7,33 @@ import axios from "axios";
  * Service creator. Hosts the references to the metadata.
  */
 export function createAtlasService() {
+  const atlas = shallowRef<Atlas | null>(null);
   const metadata = shallowRef<AtlasMetadata | null>(null);
 
   /**
-   * Build a promise that return an actual instance of the metadata.
+   * Build a promise that return an actual instance of the state.
    */
-  function metadataReady(): Promise<AtlasMetadata> {
+  function stateReady(): Promise<{
+    atlasInstance: Atlas;
+    metadataInstance: AtlasMetadata;
+  }> {
     return new Promise(resolve => {
       // Return the existing instance.
-      if (metadata.value) {
-        resolve(metadata.value);
+      if (atlas.value && metadata.value) {
+        resolve({
+          atlasInstance: atlas.value,
+          metadataInstance: metadata.value
+        });
         return;
       }
 
-      // Wait for one to become available.
+      // Wait for them to become available.
       const stop = watch(
-        metadata,
-        newMetadata => {
-          if (newMetadata) {
+        [atlas, metadata],
+        ([newAtlas, newMetadata]) => {
+          if (newAtlas && newMetadata) {
             stop();
-            resolve(newMetadata);
+            resolve({ atlasInstance: newAtlas, metadataInstance: newMetadata });
           }
         },
         { immediate: true }
@@ -39,25 +46,44 @@ export function createAtlasService() {
    *
    * Will throw errors if the metadata fetch fails or the returned data is not the right shape.
    *
-   * @param atlas Atlas to use.
+   * @param newAtlas Atlas to use.
    */
-  async function setAtlas(atlas: Atlas) {
+  async function setAtlas(newAtlas: Atlas) {
     // Reset the previous one.
+    atlas.value = null;
     metadata.value = null;
 
     // Make a new fetch.
     const metadataResponse = await axios.get<AtlasMetadata>(
-      new URL(`${atlas.name}/atlas.json`, atlas.source).toString()
+      new URL(`${newAtlas.name}/atlas.json`, newAtlas.source).toString()
     );
 
     // Update the value.
+    atlas.value = newAtlas;
     metadata.value = metadataResponse.data;
+  }
+
+  /**
+   * Return the path to the meshes for the default structures.
+   */
+  async function getDefaultStructuresMeshPaths(): Promise<string[]> {
+    const { atlasInstance, metadataInstance } = await stateReady();
+
+    return metadataInstance.structures[
+      metadataInstance.rootId
+    ]!.childrenIds.map(id =>
+      new URL(
+        `${atlasInstance.name}/meshes/${id}.glb`,
+        atlasInstance.source
+      ).toString()
+    );
   }
 
   return {
     metadata: shallowReadonly(metadata),
+    stateReady,
     setAtlas,
-    metadataReady
+    getDefaultStructuresMeshPaths
   };
 }
 
